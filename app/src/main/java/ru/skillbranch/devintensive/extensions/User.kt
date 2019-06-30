b2fca99c4f1a8be1d62a92fa046bc177a6d4c4a8
LirraSmile <nad.zyryanova@gmail.com>
2019-06-30 20:01:12 +0700

    hometask_2 first push

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -0,0 +1,24 @@
+package ru.skillbranch.devintensive.extensions
+
+import ru.skillbranch.devintensive.models.User
+import ru.skillbranch.devintensive.models.UserView
+import ru.skillbranch.devintensive.utils.Utils
+import java.util.*
+
+fun User.toUserView() : UserView {
+
+    val nickName = Utils.transliterations("$firstName $lastName")
+    val initials = Utils.toInitials(firstName, lastName)
+    val status = if (lastVisit == null) "Ещё ни разу не был" else if (isOnline) "Online" else "Last time was ${lastVisit.humanizeDiff()}"
+
+    return UserView(
+        id,
+        fullName = "$firstName $lastName",
+        avatar = avatar,
+        nickName = nickName,
+        initials = initials,
+        status = status
+    )
+}
+
+
